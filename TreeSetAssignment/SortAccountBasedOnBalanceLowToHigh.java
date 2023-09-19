c27524c2af3ecdf6160e07a0ce9b6356a5e36405
65787376 <145376420+65787376@users.noreply.github.com>
2023-09-19 23:16:01 +0530

    Add files via upload

---
+++ TreeSetAssignment/SortAccountBasedOnBalanceLowToHigh.java
@@ -0,0 +1,13 @@
+package a6;
+
+import java.util.Comparator;
+
+public class SortAccountBasedOnBalanceLowToHigh implements Comparator<Account>{
+
+	@Override
+	public int compare(Account account1, Account account2) {
+		return account1.getBalance()-account2.getBalance();
+	}
+	
+
+}
